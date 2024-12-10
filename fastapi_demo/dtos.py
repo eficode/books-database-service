e191c13319feb74a83cd390bb8adfe0127d6c449
Developer Agent <developeragent@eficode.com>
2024-12-10 10:34:18 +0000

    GenAI created implementation of the task Add Book Color Information to Books Model

--- fastapi_demo/dtos.py
+++ fastapi_demo/dtos.py
@@ -1,11 +1,17 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
 class BookCreate(BaseModel):
     title: str
     author: str
     pages: int
+    color: str  # New attribute
 
 class BookInfo(BookCreate):
     id: Optional[int] = None
+
+class BookUpdate(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    pages: Optional[int] = None
+    color: Optional[str] = None  # New attribute
