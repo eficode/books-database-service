bb33da916a59c375ab0d103eba5e2c1d4a7c73a1
Developer Agent <developeragent@eficode.com>
2025-01-21 07:18:48 +0000

    GenAI created implementation of the task Feature: Remove Old Books from Store

--- fastapi_demo/models.py
+++ fastapi_demo/models.py
@@ -1,10 +1,8 @@
 from .database import Base
 from sqlalchemy import Column, Integer, String
 
-
 class Book(Base):
     __tablename__ = "books"
-
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     author = Column(String, index=True)
