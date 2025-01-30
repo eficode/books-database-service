3a74b63bce9ad7d224cba16ac8fd6b48d0511c70
Developer Agent <developeragent@eficode.com>
2025-01-30 14:16:06 +0000

    GenAI created implementation of the task Backend Implementation Specification for 'Search for Books by Color'

--- fastapi_demo/dtos.py
+++ fastapi_demo/dtos.py
@@ -1,11 +1,17 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
 class BookCreate(BaseModel):
     title: str
     author: str
     pages: int
+    color: str  # New field for color
 
 class BookInfo(BookCreate):
     id: Optional[int] = None
+
+class BookUpdate(BaseModel):
+    title: Optional[str] = None
+    author: Optional[str] = None
+    pages: Optional[int] = None
+    color: Optional[str] = None  # New field for color
